214a770a8d82036d5d6e8787d78dc34df7155ec8
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2022-07-29 14:57:18 +0530

    os module

---
+++ os.js
@@ -0,0 +1,28 @@
+// os -> operating syatem module
+
+const a=require("os");
+
+let mySystemArch=a.arch();
+// console.log(mySystemArch);
+
+let myCPUInfo=a.cpus();
+// console.log(myCPUInfo);
+
+let hostName=a.hostname();
+// console.log(hostName);
+
+console.log(a.release());
+
+console.log(a.platform());
+
+console.log(a.freemem());
+
+console.log(a.totalmem());
+
+let myRestartTimeInHours=a.uptime()/3600;
+console.log(myRestartTimeInHours);
+
+console.log(a.userInfo())
+
+
+
