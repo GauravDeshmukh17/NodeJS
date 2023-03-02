b6e74c4b295009930e0974ca0733c63828c988de
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2023-03-02 15:48:06 +0530

    :fire: fetch

--- automation/poc/asyncAwait.js
+++ automation/poc/asyncAwait.js
@@ -14,7 +14,9 @@ f()
     })*/
 
 
-async function f(){
+    
+
+/*async function f(){
     let arr=[1,2,3,"Hello",4,5];
     return arr;
 }
@@ -27,7 +29,83 @@ f()
     })
     .catch(function(err){
         console.log(err);
+    })*/
+
+
+
+/*async function f(){
+    let promise=new Promise(function(resolve,reject){
+        setTimeout(function(){
+            resolve("done !");
+        },5000);
     })
 
+    let result=await promise // wait until promise resolves
+    console.log("hello");
+    console.log(result);  // ALERT
+}
+
+f();
+console.log("YOLO");
+*/
 
 
+
+async function showAvatar() {
+
+    // read our JSON
+    // Note : fetch runs on browser , so run this code on browser
+    let response = await fetch('https://javascript.info/article/promise-chaining/user.json');
+    let user = await response.json();
+    console.log(user);
+  
+    // read github user
+    let githubResponse = await fetch(`https://api.github.com/users/${user.name}`);
+    let githubUser = await githubResponse.json();
+  
+    // show the avatar
+    let img = document.createElement('img');
+    img.src = githubUser.avatar_url;
+    img.className = "promise-avatar-example";
+    document.body.append(img);
+  
+    // wait 3 seconds
+    // await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+    await new Promise(function(resolve,reject){
+        setTimeout(resolve, 3000);
+    })
+  
+    img.remove();
+  
+    return githubUser;
+  }
+  
+  showAvatar();
+
+
+
+  /*
+  async function showAvatar() {
+
+    // read github user 
+    // Note : fetch runs on browser , so run this code on browser
+    let githubResponse = await fetch(`https://api.github.com/users/GauravDeshmukh17`);
+    let githubUser = await githubResponse.json();
+    console.log(githubUser);  
+  
+    // show the avatar
+    let img = document.createElement('img');
+    img.src = githubUser.avatar_url;
+    img.className = "promise-avatar-example";
+    document.body.append(img);
+  
+    // // wait 3 seconds
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+  
+    img.remove();
+  
+    return githubUser;
+  }
+  
+  showAvatar();
+  */  
